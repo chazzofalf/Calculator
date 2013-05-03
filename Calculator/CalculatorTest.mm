753796a8e4845582b9b59a04f2fbc9894986e09f
Charles Montgomery <charles.montgomery@charter.net>
2013-05-03 14:00:44 -0500

    Added test case for vMAT`

--- Calculator/CalculatorTest.mm
+++ Calculator/CalculatorTest.mm
@@ -9,7 +9,13 @@
 #import <SenTestingKit/SenTestingKit.h>
 
 #import "Calculator.h"
-
+#import <vMAT.h>
+#import <Eigen/Core>
+namespace
+{
+    using namespace Eigen;
+    using namespace vMAT;
+}
 @interface CalculatorTest : SenTestCase
 {
     Calculator *calculator;
@@ -44,5 +50,28 @@
 -(void) testDivideByZero
 {
     STAssertThrows([calculator divide:5 by:0], @"We expected an exception to be raised when dividing by 0");
+}
+-(void) testVMat
+{
+    const float iniM[] = {
+        2,11,7,14,
+        3,10,6,15,
+        13,8,12,1
+    };
+    vMAT_Array * matM = [vMAT_Array arrayWithSize:vMAT_MakeSize(4,3)
+                                             type:miSINGLE
+                                             data:[NSData dataWithBytes:iniM length:sizeof(iniM)]];
+    NSLog(@"matM:\n%@",matM.dump);
+    Mat<float> M = matM;
+    Matrix<float,4,3> X = M.array() * M.array();
+    vMAT_Array * matX = vMAT_cast(X);
+    NSLog(@"matX:\n%@",matX.dump);
+    vMAT_Array * matXv = [vMAT_Array arrayWithSize:vMAT_MakeSize(4,3) type:miSINGLE];
+    Mat<float> Xv = matXv;
+    Xv << 4,9,169,
+    121,100,64,
+    49,36,144,
+    196,225,1;
+    STAssertEqualObjects(matX, matXv, @"Ensure equal arrays");
 }
 @end
